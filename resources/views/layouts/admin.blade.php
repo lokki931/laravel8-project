cd2be6ab27c08af87e9c15b5c1f0acfa5f8e7186
lokki931 <taurusr2d2@gmail.com>
2021-04-26 12:30:21 +0300

    change url css/js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,38 +8,38 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/admin//plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{ asset('/admin/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="/admin//plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="{{ asset('/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="/admin//plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="/admin//plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="{{ asset('/admin/plugins/jqvmap/jqvmap.min.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/admin//dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{ asset('/admin/dist/css/adminlte.min.css') }}">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="/admin//plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="{{ asset('/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="/admin//plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="{{ asset('/admin/plugins/daterangepicker/daterangepicker.css') }}">
   <!-- summernote -->
-  <link rel="stylesheet" href="/admin//plugins/summernote/summernote-bs4.min.css">
-  <link href="/admin/dist/css/colorbox.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('/admin/plugins/summernote/summernote-bs4.min.css">
+  <link href="/admin/dist/css/colorbox.css') }}" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="/admin//dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ asset('/admin//dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('homeAdmin')  }}" class="brand-link">
-      <img src="/admin//dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('/admin//dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminPanel</span>
     </a>
 
@@ -119,43 +119,43 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="/admin//plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('/admin//plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="/admin//plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{ asset('/admin//plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="/admin//plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('/admin//plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- ChartJS -->
-<script src="/admin//plugins/chart.js/Chart.min.js"></script>
+<script src="{{ asset('/admin//plugins/chart.js/Chart.min.js') }}"></script>
 <!-- Sparkline -->
-<script src="/admin//plugins/sparklines/sparkline.js"></script>
+<script src="{{ asset('/admin//plugins/sparklines/sparkline.js') }}"></script>
 <!-- JQVMap -->
-<script src="/admin//plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="/admin//plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ asset('/admin//plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('/admin//plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
 <!-- jQuery Knob Chart -->
-<script src="/admin//plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{ asset('/admin//plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
-<script src="/admin//plugins/moment/moment.min.js"></script>
-<script src="/admin//plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ asset('/admin//plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('/admin//plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="/admin//plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{ asset('/admin//plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
-<script src="/admin//plugins/summernote/summernote-bs4.min.js"></script>
+<script src="{{ asset('/admin//plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="/admin//plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ asset('/admin//plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="/admin//dist/js/adminlte.js"></script>
+<script src="{{ asset('/admin//dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="/admin//dist/js/demo.js"></script>
+<script src="{{ asset('/admin//dist/js/demo.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="/admin//dist/js/pages/dashboard.js"></script>
-<script type="text/javascript" src="/admin/dist/js/jquery.colorbox-min.js"></script>
-<script type="text/javascript" src="/packages/barryvdh/elfinder/js/standalonepopup.js"></script>
+<script src="{{ asset('/admin//dist/js/pages/dashboard.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/admin/dist/js/jquery.colorbox-min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/packages/barryvdh/elfinder/js/standalonepopup.js') }}"></script>
 <script src="https://cdn.tiny.cloud/1/evae9atugis496ar3jaxt8ry120anq3bp5jlmd6kquw8agvf/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
-<script src="/admin//admin.js"></script>
+<script src="{{ asset('/admin//admin.js') }}"></script>
 </body>
 </html>
